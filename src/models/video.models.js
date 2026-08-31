aaaf9cc67385d7a0e112198775832d3fc5d3bfee
akash12576 <ks221457@gmail.com>
2026-08-31 22:52:52 +0530

    feat: implement healthcheck route and response structure with error handling

---
+++ src/models/video.models.js
@@ -0,0 +1,56 @@
+/* 
+Owner ObjectId users
+    videoFile string
+    thumbnail string
+    title string
+    description string
+    duration number
+    views number
+    isPublished boolean
+    createdAt date
+    Updated Date
+*/
+
+
+import mongoose , { Schema} from "mongoose";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, //cloudnary URL
+        required: true
+    },
+    thumbnail: {
+        type: String, //cloudnary URL
+        required: true   
+    },
+    title: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true  
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+} , { timestamps: true}
+)
+
+
+
+
+export const Video = mongoose.model("Video", videoSchema)
